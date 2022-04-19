2632c7bc5a27c52cc220009787fdc8ab562a0c77
karrimDCI <karim_afettouche@hotmail.com>
2022-04-19 14:42:21 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as Sentry from "@sentry/react";
+import { ToastContainer } from "react-toastify";
+import http from "./services/httpService";
+import config from "./config.json";
+import logo from "./logo.svg";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data: posts } = await http.get(config.apiEndpoint);
+
+      setPosts(posts);
+    };
+    getData();
+  }, []);
+
+  const handleAdd = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await http.post(config.apiEndpoint, obj);
+
+    const addPosts = [post, ...posts];
+    setPosts(addPosts);
+  };
+
+  const handleUpdate = async (post) => {
+    post.title = "UPDATED";
+    const { data } = await http.put(`${config.apiEndpoint}/${post.id}`, post);
+    // axios.patch(`${apiEndpoint}${post.id}`, { title: post.title });
+    const updatePosts = [...posts];
+    const index = updatePosts.indexOf(post);
+    posts[index] = { ...post };
+    setPosts(updatePosts);
+  };
+
+  const handleDelete = async (post) => {
+    //Optimistic update
+    // 1- keep a reference to orignal state
+    const originalPosts = posts;
+
+    // 2- you update the url before calling the server
+    const deletePost = posts.filter((p) => p.id !== post.id);
+    setPosts(deletePost);
+
+    // 3- wrap the call to the server in try & catch block
+    try {
+      await http.delete(`s${config.apiEndpoint}/${post.id}`);
+      // throw new Error("");
+    } catch (err) {
+      if (err.response && err.response.status === 404)
+        alert(`This post has already been deleted`);
+
+      setPosts(originalPosts);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ToastContainer />
+      <button className="btn btn-primary" onClick={handleAdd}>
+        Add
+      </button>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.title}</td>
+              <td>
+                <button
+                  className="btn btn-info btn-sm"
+                  onClick={() => handleUpdate(post)}
+                >
+                  Update
+                </button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleDelete(post)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn: "https://9c2735b7cb2840c19b17250158957a66@o1152267.ingest.sentry.io/6346570",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
